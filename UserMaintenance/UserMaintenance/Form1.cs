078e4bd8f530a2e94381656af72ae54dfcad041f
corvinus-ewhn1v <aron.szabolcsi@stud.uni-corvinus.hu>
2021-10-01 12:15:12 +0200

    Fájlba írás

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,7 @@ namespace UserMaintenance
 
             label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.Write;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -34,6 +36,31 @@ namespace UserMaintenance
             };
 
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "Comma Seperated Values (*.csv) | *.csv";
+            sfd.DefaultExt = "csv";
+            sfd.AddExtension = true;
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            using (var sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8)) 
+            {
+                foreach (var u in users)
+                {
+                    sw.WriteLine(string.Format(
+                        "{0}{1}",
+                        u.ID,
+                        u.FullName
+                        ));
+                }
+            }
+
         }
     }
 }
