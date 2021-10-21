2641ae34db3097e9757c6a4569cbd2d75f7db74c
corvinus-ewhn1v <aron.szabolcsi@stud.uni-corvinus.hu>
2021-10-21 09:25:59 +0200

    Fájlba mentés

--- UserMaintenance/5.gyak/Form1.cs
+++ UserMaintenance/5.gyak/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ namespace _5.gyak
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
         List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        List<decimal> Nyereségek = new List<decimal>();
         public Form1()
         {
             InitializeComponent();
@@ -23,7 +25,7 @@ namespace _5.gyak
             dataGridView1.DataSource = Ticks;
             CreatePortfolio();
 
-            List<decimal> Nyereségek = new List<decimal>();
+
             int intervalum = 30;
             DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
             DateTime záróDátum = new DateTime(2016, 12, 30);
@@ -39,8 +41,9 @@ namespace _5.gyak
             var nyereségekRendezve = (from x in Nyereségek
                                       orderby x
                                       select x)
-                                        .ToList();
+                                         .ToList();
             MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
+
         }
 
         private void CreatePortfolio()
@@ -65,6 +68,28 @@ namespace _5.gyak
                 value += (decimal)last.Price * item.Volume;
             }
             return value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "Text files (*.txt) | *.txt | All files (*.*) | *.*";
+            sfd.DefaultExt = "txt";
+            sfd.AddExtension = true;
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            using (var sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                sw.WriteLine("Időszak\tNyereség");
+                for (int i = 0; i < Nyereségek.Count; i++)
+                {
+                    sw.WriteLine((i + 1).ToString() + "\t" + Nyereségek[i]);
+                }
+
+            }
+
         }
     }
 }
