a5d70c463ff8501a4f60e8cf4294035f3c173017
corvinus-ewhn1v <aron.szabolcsi@stud.uni-corvinus.hu>
2021-10-28 15:31:34 +0200

    Webszolgáltatás hívása

--- UserMaintenance/6.gyak/Form1.cs
+++ UserMaintenance/6.gyak/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _6.gyak.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace _6.gyak
 {
     public partial class Form1 : Form
@@ -15,6 +18,23 @@ namespace _6.gyak
         public Form1()
         {
             InitializeComponent();
+            GetExchangeRates();
+        }
+
+        private void GetExchangeRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames= "EUR",
+                startDate= "2020-01-01",
+                endDate= "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
