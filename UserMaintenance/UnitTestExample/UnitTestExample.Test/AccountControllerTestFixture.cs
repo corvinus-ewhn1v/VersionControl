ae25605a92dbc563543ee15f07dc11ffaecb5d71
corvinus-ewhn1v <aron.szabolcsi@stud.uni-corvinus.hu>
2021-11-24 16:42:37 +0100

    E-mail validálás tesztesetek

--- UserMaintenance/UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UserMaintenance/UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -10,17 +10,25 @@ namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
     {
-        [Test]
-        private void TestValidateEmail(string email, bool expectedResult)
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("irf@uni-corvinus", false),
+            TestCase("irf.uni-corvinus.hu", false),
+            TestCase("irf@uni-corvinus.hu", true)
+        ]
+
+        public void TestValidateEmail(string email, bool expectedResult)
         {
             // Arrange
-            var accountController= new AccountController();
+            var accountController = new AccountController();
 
             // Act
             var actualResult = accountController.ValidateEmail(email);
 
             // Assert
             Assert.AreEqual(expectedResult, actualResult);
+
         }
     }
 }
